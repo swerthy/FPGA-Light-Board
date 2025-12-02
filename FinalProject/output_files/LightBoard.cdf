4461951c171ad45110ed8b62150b9df1704beb63
Shane Werthaiser <sdw4545@gmail.com>
2025-12-02 18:59:30 -0500

    Open Folder Upload
    Uploaded open folder to GitHub log to record all further modifications to one file rather than several zipped folders

---
+++ FinalProject/output_files/LightBoard.cdf
@@ -0,0 +1,15 @@
+/* Quartus Prime Version 23.1std.1 Build 993 05/14/2024 SC Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Ign)
+		Device PartName(5CSEMA5F31) MfrSpec(OpMask(0) FullPath("M:/ECE287/FinalProject/output_files/LightBoard.sof"));
+	P ActionCode(Cfg)
+		Device PartName(5CSEMA5F31) Path("M:/ECE287/FinalProject/output_files/") File("LightBoard.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ FinalProject/packet.v
@@ -0,0 +1,127 @@
+module packet(
+
+	input clk,
+	input rst,
+	input send,
+	input [5642:0]data,
+	output reg pos,
+	output reg done
+
+);
+
+reg [15:0]count;
+reg [7:0]bitcount;
+reg [5642:0]sends;
+
+reg [2:0]S;
+reg [2:0]NS;
+
+parameter 
+	BREAK=3'd0, //Break between DMX packets
+	MARK1=3'd1, //Start of a packet
+	DATA=3'd2,  //Bits of a packet
+	NEXT=3'd3,  //Move to next data bit
+	MARK2=3'd4, //End of a packet
+	DONE=3'd5,	//Done signal
+	ERROR=3'hF; //Error
+	
+always@(posedge clk or negedge rst)
+	if(rst==1'b0)
+		S <= BREAK;
+	else
+		S <= NS;
+
+always@(*)
+	case(S)
+		
+		BREAK:
+		if(send==0)
+			NS <= BREAK;
+		else
+			NS <= MARK1;
+		
+		MARK1:
+			if(count<50000) //1ms
+				NS <= MARK1;
+			else
+				NS <= DATA;
+
+		DATA:
+			if(sends==5643'd0) //All data sent
+				NS <= MARK2;
+			else
+			begin
+				if(bitcount<200) //4us
+					NS <= DATA;
+				else
+					NS <= NEXT;
+			end
+		
+		NEXT: NS <= DATA;
+
+		MARK2:
+			if(count<25000) //0.5ms
+				NS <= MARK2;
+			else
+				NS <= DONE;
+		
+		DONE: NS <= BREAK;
+		
+	endcase
+
+always@(posedge clk or negedge rst)
+if(rst==1'b0)
+begin
+		count <= 0;
+		bitcount <= 0;
+		sends <= data;
+		pos <= 0;
+		neg <= 0;
+		done <= 0;
+end
+else
+	case(S)
+	
+		BREAK: 
+		begin
+			count <= 0;
+			bitcount <= 0;
+			sends <= data;
+			  <= 0;
+			neg <= 0;
+		end
+		
+		MARK1:
+		begin
+			pos <= 1;
+			count <= count+1'b1;
+		end
+		
+		DATA:
+		begin
+			pos <= sends[0];
+			count <= 0;
+			bitcount <= bitcount+1'b1;
+		end
+		
+		NEXT: 
+		begin
+			sends <= sends/2;
+			bitcount <= 0;
+		end
+		
+		MARK2:
+		begin
+			pos <= 1;
+			count <= count+1'b1;
+		end
+		
+		DONE:
+		begin
+			pos <= 1;
+			done <= 1;
+		end
+		
+	endcase
+
+endmodule
